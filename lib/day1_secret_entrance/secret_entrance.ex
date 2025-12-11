47af9c702b26d1ca0046a7783bc9f0f51ad79c59
Frederick Morin <freddydumont@users.noreply.github.com>
2025-12-11 15:43:24 -0500

    refactor: extract new dial function

--- lib/day1_secret_entrance/secret_entrance.ex
+++ lib/day1_secret_entrance/secret_entrance.ex
@@ -50,22 +50,18 @@ defmodule SecretEntrance do
     {direction, number} = String.split_at(line, 1)
     value = String.to_integer(number)
 
-    rotations_past_0 =
-      case direction do
-        "R" ->
-          div(dial + value, 100)
+    {
+      calculate_new_dial(value, direction, dial),
+      calculate_rotations(direction, dial, value) + count
+    }
+  end
 
-        "L" when dial - value == 0 ->
-          1
-
-        "L" when dial - value < 0 ->
-          (div(dial - value, 100) |> abs()) + if(dial != 0, do: 1, else: 0)
-
-        _ ->
-          0
-      end
-
-    new_dial = calculate_new_dial(value, direction, dial)
-    {new_dial, count + rotations_past_0}
+  defp calculate_rotations(direction, dial, value) do
+    case {direction, dial - value} do
+      {"R", _} -> div(dial + value, 100)
+      {"L", 0} -> 1
+      {"L", n} when n < 0 -> (div(n, 100) |> abs()) + if(dial != 0, do: 1, else: 0)
+      _ -> 0
+    end
   end
 end
