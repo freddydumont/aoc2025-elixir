e363febb802c8f6c374054ac8653c61c938fad3c
Frederick Morin <freddydumont@users.noreply.github.com>
2025-12-11 15:35:25 -0500

    Implement working part 2

--- lib/day1_secret_entrance/secret_entrance.ex
+++ lib/day1_secret_entrance/secret_entrance.ex
@@ -13,27 +13,59 @@ defmodule SecretEntrance do
     |> elem(1)
   end
 
-  @doc """
-  Counts the number of time the dial goes past 0, whether during or at the end of
-  a rotation. Same dial functionality as above.
-  """
-  def count_dial_past_zero(_path, _dial \\ 50, count \\ 0) do
-    count
-  end
-
   defp parse_line(line, dial, count) do
     {direction, number} = String.split_at(line, 1)
-    value = String.to_integer(number)
 
     new_dial =
-      case direction do
-        "R" -> rem(dial + value, 100)
-        "L" -> rem(dial - value + 100, 100)
-      end
+      String.to_integer(number)
+      |> calculate_new_dial(direction, dial)
 
     {new_dial, increment_if_zero(count, new_dial)}
   end
 
+  defp calculate_new_dial(rotate_by, "R", dial), do: rem(dial + rotate_by, 100)
+
+  defp calculate_new_dial(rotate_by, "L", dial),
+    do: rem(dial - rotate_by + 100, 100) |> normalize_dial()
+
+  defp normalize_dial(dial) when dial < 0, do: 100 + dial
+  defp normalize_dial(dial), do: dial
   defp increment_if_zero(count, 0), do: count + 1
   defp increment_if_zero(count, _), do: count
+
+  @doc """
+  Counts the number of time the dial is on 0, whether during a rotation or at the end of one.
+  Same dial functionality as above.
+  """
+  def count_dial_past_zero(path, dial \\ 50, count \\ 0) do
+    File.stream!(path, [:trim_bom, encoding: :utf8])
+    |> Stream.map(&String.trim/1)
+    |> Enum.reduce({dial, count}, fn line, {dial, count} ->
+      parse_line_part_2(line, dial, count)
+    end)
+    |> elem(1)
+  end
+
+  defp parse_line_part_2(line, dial, count) do
+    {direction, number} = String.split_at(line, 1)
+    value = String.to_integer(number)
+
+    rotations_past_0 =
+      case direction do
+        "R" ->
+          div(dial + value, 100)
+
+        "L" when dial - value == 0 ->
+          1
+
+        "L" when dial - value < 0 ->
+          (div(dial - value, 100) |> abs()) + if(dial != 0, do: 1, else: 0)
+
+        _ ->
+          0
+      end
+
+    new_dial = calculate_new_dial(value, direction, dial)
+    {new_dial, count + rotations_past_0}
+  end
 end
--- test/secret_entrance_test.exs
+++ test/secret_entrance_test.exs
@@ -2,12 +2,21 @@ defmodule SecretEntranceTest do
   use ExUnit.Case
   doctest SecretEntrance
 
-  test "returns the correct count for part 1" do
+  test "returns the correct count for part 1 limited" do
     assert SecretEntrance.count_dial_at_zero("lib/day1_secret_entrance/inputs/day1_test.txt") == 3
   end
 
-  test "returns the correct count for part 2" do
+  test "returns the correct count for part 1 full" do
+    assert SecretEntrance.count_dial_at_zero("lib/day1_secret_entrance/inputs/day1.txt") == 1129
+  end
+
+  test "returns the correct count for part 2 limited" do
     assert SecretEntrance.count_dial_past_zero("lib/day1_secret_entrance/inputs/day1_test.txt") ==
              6
+  end
+
+  test "returns the correct count for part 2 full" do
+    assert SecretEntrance.count_dial_past_zero("lib/day1_secret_entrance/inputs/day1.txt") ==
+             6638
   end
 end
