aa97c08e5a73a08c8145fbb5bd15202efd946a52
Frederick Morin <freddydumont@users.noreply.github.com>
2025-12-11 17:13:03 -0500

    clean up folder for day 1

--- test/secret_entrance_test.exs
+++ test/secret_entrance_test.exs
@@ -3,22 +3,22 @@ defmodule SecretEntranceTest do
   doctest SecretEntrance
 
   test "returns the correct count for part 1 limited" do
-    assert SecretEntrance.count_zeros(:at_zero, "lib/day1_secret_entrance/inputs/day1_test.txt") ==
+    assert SecretEntrance.count_zeros(:at_zero, "lib/01_secret_entrance/input_test.txt") ==
              3
   end
 
   test "returns the correct count for part 1 full" do
-    assert SecretEntrance.count_zeros(:at_zero, "lib/day1_secret_entrance/inputs/day1.txt") ==
+    assert SecretEntrance.count_zeros(:at_zero, "lib/01_secret_entrance/input.txt") ==
              1129
   end
 
   test "returns the correct count for part 2 limited" do
-    assert SecretEntrance.count_zeros(:past_zero, "lib/day1_secret_entrance/inputs/day1_test.txt") ==
+    assert SecretEntrance.count_zeros(:past_zero, "lib/01_secret_entrance/input_test.txt") ==
              6
   end
 
   test "returns the correct count for part 2 full" do
-    assert SecretEntrance.count_zeros(:past_zero, "lib/day1_secret_entrance/inputs/day1.txt") ==
+    assert SecretEntrance.count_zeros(:past_zero, "lib/01_secret_entrance/input.txt") ==
              6638
   end
 end
