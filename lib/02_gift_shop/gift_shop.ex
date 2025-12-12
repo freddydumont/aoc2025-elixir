9a2d574f7b18a7b329335c8234f930da8a755313
Frederick Morin <freddydumont@users.noreply.github.com>
2025-12-12 16:22:44 -0500

    Setup parsing mode for day 2

--- lib/02_gift_shop/gift_shop.ex
+++ lib/02_gift_shop/gift_shop.ex
@@ -7,15 +7,15 @@ defmodule GiftShop do
   """
   require Integer
 
-  def validate_IDs(path) do
+  def validate_IDs(mode, path) do
     path
     |> File.read!()
     |> String.split(",", trim: true)
-    |> Enum.reduce(0, fn range, sum -> parse_range(range, sum) end)
+    |> Enum.reduce(0, fn range, sum -> parse_range(range, sum, mode) end)
   end
 
   # Parses the given range and add invalid IDs to sum
-  defp parse_range(range, sum) do
+  defp parse_range(range, sum, mode) do
     [first_id, last_id] = String.split(range, "-")
     range_start = String.to_integer(first_id)
     range_stop = String.to_integer(last_id)
@@ -26,20 +26,26 @@ defmodule GiftShop do
       as_string = Integer.to_string(id)
       char_count = String.length(as_string)
 
-      # part 2 is any repeating pattern
-      # we'll need to check for patterns of size: 1 to div(char_count, 2)
-      # for a char_count of 7 we'd check from 1 to 3
-      # for a char_count of 15 we'd check from 1 to 7
-      # ? for pattern 1 we only need to check that all chars are equal (no need to bother as below takes care of it)
-      # for pattern 2+ we have to split at specified chars, and check equality for all parts
-      # all of this can be handled by creating a regular expression from the part we want to match,
-      # and checking for matches: 123123 -> /(123){2}/
+      case mode do
+        :exact_half ->
+          if Integer.is_odd(char_count) do
+            sum
+          else
+            {first_part, second_part} = String.split_at(as_string, div(char_count, 2))
+            if first_part == second_part, do: sum + id, else: sum
+          end
 
-      if Integer.is_odd(char_count) do
-        sum
-      else
-        {first_part, second_part} = String.split_at(as_string, div(char_count, 2))
-        if first_part == second_part, do: sum + id, else: sum
+        :any_pattern ->
+          # part 2 is any repeating pattern
+          # we'll need to check for patterns of size: 1 to div(char_count, 2)
+          # for a char_count of 7 we'd check from 1 to 3
+          # for a char_count of 15 we'd check from 1 to 7
+          # ? for pattern 1 we only need to check that all chars are equal (no need to bother as below takes care of it)
+          # for pattern 2+ we have to split at specified chars, and check equality for all parts
+          # all of this can be handled by creating a regular expression from the part we want to match,
+          # and checking for matches: 123123 -> /(123){2}/
+
+          sum
       end
     end)
   end
--- test/gift_shop_test.exs
+++ test/gift_shop_test.exs
@@ -3,18 +3,18 @@ defmodule GiftShopTest do
   doctest GiftShop
 
   test "[Part 1] example input returns the invalid IDs total" do
-    assert GiftShop.validate_IDs("lib/02_gift_shop/input_test.txt") == 1_227_775_554
+    assert GiftShop.validate_IDs(:exact_half, "lib/02_gift_shop/input_test.txt") == 1_227_775_554
   end
 
   test "[Part 1] full input returns the invalid IDs total" do
-    assert GiftShop.validate_IDs("lib/02_gift_shop/input.txt") == 16_793_817_782
+    assert GiftShop.validate_IDs(:exact_half, "lib/02_gift_shop/input.txt") == 16_793_817_782
   end
 
   test "[Part 2] example input returns the invalid IDs total" do
-    assert GiftShop.validate_IDs("lib/02_gift_shop/input_test.txt") == 4_174_379_265
+    assert GiftShop.validate_IDs(:any_pattern, "lib/02_gift_shop/input_test.txt") == 4_174_379_265
   end
 
   test "[Part 2] full input returns the invalid IDs total" do
-    assert GiftShop.validate_IDs("lib/02_gift_shop/input_test.txt") == 0
+    assert GiftShop.validate_IDs(:any_pattern, "lib/02_gift_shop/input_test.txt") == 0
   end
 end
